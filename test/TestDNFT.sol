981114972e178e9c1e92600959ababeaeef5bb6b
qTeePie <isabel@chaparian.no>
2025-03-24 22:21:51 +0100

    DNFT Test ✅ 	- Tests that we can pass in an address and get it approved as marketplace ✨

--- test/TestDNFT.sol
+++ test/TestDNFT.sol
@@ -7,29 +7,37 @@ import {DNFT} from "contracts/DNFT.sol";
 import {DeployDNFT} from "script/DeployDNFT.s.sol";
 
 contract TestDNFT is Test {
-    string TOKEN_URI = "ipfs://test-uri";
+    string constant TOKEN_URI = "ipfs://test-uri";
+
+    address immutable user = makeAddr("user");
+    address immutable marketplace = makeAddr("marketplace");
 
     DNFT dnft;
-
-    address user = address(1);
 
     function setUp() public {
         //DeployDNFT deployer = new DeployDNFT();
         //deployer.run();
+
+        // Initialize DNFT
         dnft = new DNFT();
         console2.log("Workie");
     }
 
     function testMint() public {
+        // Arrange Act
         dnft.mintNFT(user, "ipfs://test-uri");
-        assertEq(dnft.ownerOf(0), address(1), "Owner should be correct");
+
+        // Assert
+        assertEq(dnft.ownerOf(0), user, "Owner should be correct");
     }
 
-    function testTokenIdIncrements() public {
-        // Mint the first NFT
-        string memory tokenURI = TOKEN_URI;
-        dnft.mintNFT(user, tokenURI);
-    }
+    function testCanApproveMarketplace() public {
+        // Arrange
+        dnft.mintNFT(user, TOKEN_URI);
+        vm.prank(user);
 
-    function testCanApproveMarketplace() public {}
+        // Act
+        dnft.approveMarketplace(marketplace, 0);
+        assertEq(dnft.getApproved(0), marketplace);
+    }
 }
